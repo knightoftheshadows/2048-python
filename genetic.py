4c8a6fd871ff771ae6b99200e5582a028da700d1
Elrubo <oftheshadowsknight@gmail.com>
2020-04-21 16:36:09 -0400

    modifying neural network

--- genetic.py
+++ genetic.py
@@ -4,46 +4,8 @@ import random
 import torch
 import torch.nn as nn
 
-
-
-class Network(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        # Inputs to hidden layer linear transformation
-        self.hidden = nn.Linear(c.GRID_LEN*c.GRID_LEN, 4*c.GRID_LEN*c.GRID_LEN)
-        #another hidden layer
-        self.hidden2 = nn.Linear(4*c.GRID_LEN*c.GRID_LEN, 256)
-        # Output layer, 4 units = one for each direction
-        self.output = nn.Linear(256, 4)
-
-        # Define sigmoid activation and softmax output
-        self.sigmoid = nn.Sigmoid()
-        self.sigmoid2 = nn.Sigmoid()
-        self.sigmoid3 = nn.Sigmoid()
-
-    def forward(self, x):
-        #preprocessing
-        y = []
-        for i in x:
-            for e in i:
-                y.append(float(e))
-        x = y
-        x = torch.tensor(x)
-        #pass tensor through each layer
-        x = self.hidden(x)
-        x = self.sigmoid(x)
-        x = self.hidden2(x)
-        x = self.sigmoid2(x)
-        x = self.output(x)
-        x = self.sigmoid3(x)
-        x = x.tolist().index(max(x.tolist())) #returns the index which has max probability
-
-        #transforms index into char
-        valid_chars = [c.KEY_LEFT, c.KEY_DOWN, c.KEY_RIGHT, c.KEY_UP]
-
-        return valid_chars[x]
-
+'''
+#fake neural network used for testing
 def fakenn(mat):
     if (random.random()  > 0.75):
         return 'a'
@@ -53,9 +15,43 @@ def fakenn(mat):
         return 'd'
     else:
         return 'w'
+'''
+'''
+    def forward(self, x, net):
+        #preprocessing
+        y = []
+        for i in x:
+            for e in i:
+                y.append(float(e))
+        x = y
+        x = torch.tensor(x)
+        #pass tensor through each layer
+        net.
+        x = x.tolist().index(max(x.tolist())) #returns the index which has max probability
 
+        #transforms index into char
+        valid_chars = [c.KEY_LEFT, c.KEY_DOWN, c.KEY_RIGHT, c.KEY_UP]
+
+        return valid_chars[x]
+'''
+'''
 while(str(p.EXITCODE) != str(22)):
     nn = Network()
+    #nn.setWeights()
     OBJ = p.GameGrid(nn.forward)
     print(p.EXITCODE)
+'''
+layers = []
+layers.append(nn.Linear(c.GRID_LEN*c.GRID_LEN, 4*c.GRID_LEN*c.GRID_LEN))
+layers.append(nn.Sigmoid())
+layers.append(nn.Linear(4*c.GRID_LEN*c.GRID_LEN, 4))
+layers.append(nn.Sigmoid())
 
+net = nn.Sequential(*layers)
+
+with torch.no_grad():
+    net[0].weight = nn.Parameter(torch.ones_like(net[0].weight))
+    net[0].weight[0, 0] = 2.
+    net[0].weight.fill_(3.)
+
+print(net)
